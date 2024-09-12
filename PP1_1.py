8a092a8bdf42aded5890ec2272f594cc178d2a50
Renu-Nibha-Ramamoorthi-ICD2O1-O2 <renunibha.ramamoorthi@student.tdsb.on.ca>
2024-09-12 13:48:46 -0400

    Update PP1_1.py

--- PP1_1.py
+++ PP1_1.py
@@ -2,16 +2,16 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-
+print("ICD2O1")
 def q2():
   #Write Assignment code here
-  
+ Print("ICD2O1") 
 def q3():
   #Write Assignment code here
-  
+ print("ICD2O1")
 def q4():
   #Write Assignment code here
-
+ print("ICD2O1")
 #Do not alter anything after this line
 q1()
 q2()
